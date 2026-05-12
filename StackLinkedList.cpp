476bd90bae30eebe70e583625e43570ba7c55b5f
Maman <ingratarmanrizky19@gmail.com>
2026-05-12 11:27:38 +0700

    case 4 terakhhir

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -116,5 +116,12 @@ while (choice !=5)
             cout << "Stuck is empty. No top value." << endl;
         }
         break;
+    case 4:
+        cout << "Exiting program." << endl;
+        break;
+    default:
+        cout << "Invalid choice. Please try again." <<endl;
+        break;
     
+     
     
